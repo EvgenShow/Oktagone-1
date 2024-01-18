9b2ab5d71249dfdaf8483c70a989978b95abf9a8
EvgenShow <prtimm602@gmail.com>
2024-01-18 12:52:42 +0800

    2

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ app.get('/', (req, res) => {
   res.send('<h1>Привет, Октагон!</h1>');
 });
 
+app.get('/static', (req, res) => {
+  res.json({ header: 'Hello', body: 'Octagon NodeJS Test' });
+});
+
+app.get('/dynamic', (req, res) => {
+  const a = parseFloat(req.query.a);
+  const b = parseFloat(req.query.b);
+  const c = parseFloat(req.query.c);
+
+  if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    res.json({ header: 'Error' });
+  } else {
+    const result = (a * b * c) / 3;
+    res.json({ header: 'Calculated', body: result.toString() });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Сервер запущен по адресу http://localhost:${port}`);
 });
