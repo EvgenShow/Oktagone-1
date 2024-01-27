259bdd9b79d5c1f7d130a27c3a20ae1ddf3c0ddb
EvgenShow <prtimm602@gmail.com>
2024-01-27 16:28:58 +0800

    Задание 5

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 const mysql = require('mysql2');
 const TelegramBot = require('node-telegram-bot-api');
 
-// Создаем соединение с базой данных
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -20,12 +20,12 @@ connection.connect((err) => {
   console.log('Подключено к базе данных MySQL');
 });
 
-// Подключаем middleware для работы с JSON в запросах
+
 app.use(express.json()); 
 
-// Определяем роуты для работы с базой данных
 
-// GET запрос для получения всех элементов
+
+
 app.get('/getAllItems', (req, res) => {
   connection.query('SELECT * FROM Items', (error, results, fields) => {
     if (error) {
@@ -37,7 +37,7 @@ app.get('/getAllItems', (req, res) => {
   });
 });
 
-// POST запрос для добавления нового элемента
+
 app.post('/addItem', (req, res) => {
   const { name, desc } = req.body;
   if (!name || !desc) {
@@ -54,7 +54,6 @@ app.post('/addItem', (req, res) => {
   });
 });
 
-// POST запрос для удаления элемента
 app.post('/deleteItem', (req, res) => {
   const { id } = req.body;
   if (!id) {
@@ -71,7 +70,7 @@ app.post('/deleteItem', (req, res) => {
   });
 });
 
-// POST запрос для обновления элемента
+
 app.post('/updateItem', (req, res) => {
   const { id, name, desc } = req.body;
   if (!id || !name || !desc) {
@@ -88,17 +87,42 @@ app.post('/updateItem', (req, res) => {
   });
 });
 
-// Подключаем Telegram бот
+
 const token = '6292582336:AAHkYmAIL1Tn5lPr0N_Q9dV6srA2CpD9SyI';
 const bot = new TelegramBot(token, { polling: true });
 
-// Обработка команды /start
+
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Привет, Октагон!');
+  bot.sendMessage(chatId, 'Привет, Октагон! Напишите /help для получения подробной информации');
 });
 
-// Запуск сервера
+
+bot.onText(/\/help/, (msg) => {
+  const chatId = msg.chat.id;
+  const message = `
+Список доступных команд:
+/help - Выводит список команд с описанием
+/site - Отправляет ссылку на сайт Октагона
+/creator - Отправляет информацию о создателе бота
+`;
+  bot.sendMessage(chatId, message);
+});
+
+
+bot.onText(/\/site/, (msg) => {
+  const chatId = msg.chat.id;
+  const message = 'Ссылка на сайт Октагона: https://students.forus.ru';
+  bot.sendMessage(chatId, message);
+});
+
+bot.onText(/\/creator/, (msg) => {
+  const chatId = msg.chat.id;
+  const message = 'Мой создатель: Байсаров Евгений Яковлевич';
+  bot.sendMessage(chatId, message);
+});
+
+
 app.listen(port, () => {
   console.log(`Сервер запущен по адресу http://localhost:${port}`);
 });
